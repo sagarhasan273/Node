501eeca433373fe7b799c1578266eafbb316afca
sagarhasan273 <sagarhasan273@gmail.com>
2023-08-14 15:52:05 +0600

    Global Object & Module System

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const people = require('./src/people');
+const { people, value } = require('./src/people');
 
-console.log(_.last(people.people));
-console.log(people.value);
+console.log(_.last(people));
+console.log(value);
