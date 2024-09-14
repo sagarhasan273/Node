5f05c7af6b8eff68db677c75854214adf0ec36a3
sagarhasan273 <sagarhasan273@gmail.com>
2024-09-15 02:35:30 +0600

    sorting numbers correctly

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const myArray = [1, 2, 3];
+const myArray = [11, 20, 13, 10, 5, 30];
 console.log(myArray);
 
-const myArray1 = new Array(5);
-console.log(myArray1);
+// const myArray1 = new Array(5);
+// console.log(myArray1);
 
-const myArray2 = new Array(1, 2, 3);
-console.log(myArray2);
+// const myArray2 = new Array(1, 2, 3);
+// console.log(myArray2);
 
-console.log(myArray.push(5))
-console.log(myArray);
+// console.log(myArray.push(5))
+// console.log(myArray);
 
-console.log(myArray.pop())
-console.log(myArray);
+// console.log(myArray.pop())
+// console.log(myArray);
 
-console.log(myArray.shift())
-console.log(myArray);
+// console.log(myArray.shift())
+// console.log(myArray);
 
-console.log(myArray.unshift(1))
+// console.log(myArray.unshift(1))
+// console.log(myArray);
+
+console.log(myArray.sort((a, b) => b - a));
 console.log(myArray);
