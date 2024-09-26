a14a1f4b8b1ef775e7065ab97e5ca445f9007563
sagarhasan273 <sagarhasan273@gmail.com>
2024-09-27 02:19:07 +0600

    object function property

--- index.js
+++ index.js
@@ -1,53 +1,8 @@
-function computeLPS(pattern) {
-    const m = pattern.length;
-    const lps = new Array(m).fill(0);
-    let length = 0;
-    let i = 1;
-
-    while (i < m) {
-        if (pattern[i] === pattern[length]) {
-            length++;
-            lps[i] = length;
-            i++;
-        } else {
-            if (length !== 0) {
-                length = lps[length - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
+let john = {
+    name: "John",
+    sayHi: function() {
+      alert("Hi buddy!");
     }
-    return lps;
-}
-
-function kmpSearch(text, pattern) {
-    const n = text.length;
-    const m = pattern.length;
-    const lps = computeLPS(pattern);
-
-    let i = 0; // index for text
-    let j = 0; // index for pattern
-    while (i < n) {
-        if (pattern[j] === text[i]) {
-            i++;
-            j++;
-        }
-
-        if (j === m) {
-            console.log(`Pattern found at index ${i - j}`);
-            j = lps[j - 1];
-        } else if (i < n && pattern[j] !== text[i]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-}
-
-// Test
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-kmpSearch(text, pattern);
+  };
+  
+  john.sayHi(); // Hi buddy!
