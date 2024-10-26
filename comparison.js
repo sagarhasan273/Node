56bb711f57e49973650c936139888e4b95feefbc
sagarhasan273 <sagarhasan273@gmail.com>
2024-10-26 23:01:32 +0600

    true === 1

--- comparison.js
+++ comparison.js
@@ -1,5 +1,5 @@
-// console.log(1 === '1');
-console.log(true === 1);
+console.log(1 === '1');
+// console.log(true === 1);
 console.log(null === undefined);
 console.log('' === false);
 console.log(NaN === NaN);
