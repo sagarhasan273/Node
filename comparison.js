bd20e20805824f51d00f772ad3c09bb8a41a7649
sagarhasan273 <sagarhasan273@gmail.com>
2024-11-04 23:02:06 +0600

    comparison 15

--- comparison.js
+++ comparison.js
@@ -12,7 +12,7 @@ console.log(11, null == undefined);
 console.log(12, '' == false);
 console.log(13, '0' == false);
 console.log(14, [1, 2, 3] == '1,2,3');
-// console.log(15, [] == false);
+console.log(15, [] == false);
 console.log(16, null == 0);
 console.log(17, null == false);
 console.log(18, [0] == false);
