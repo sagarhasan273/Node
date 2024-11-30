20e37b6b27bde007b90a2a5e3bd5906099fe89d8
sagarhasan273 <sagarhasan273@gmail.com>
2024-12-01 00:11:43 +0600

    promise than catch

--- promises.js
+++ promises.js
@@ -1,10 +1,7 @@
 let promise = new Promise((resolve) => {
-  setTimeout(() => resolve('solved'), 1000);
+  resolve('solved');
 })
 .then((value) => console.log('Promise is '+ value))
-.finally(() => {
-  console.log(1/0)
-  throw new Error("error");
-}).catch(err => console.log(err))
+.catch(err => console.log(err))
 
 console.log(promise);
